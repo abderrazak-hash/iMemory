70832b162df017754ee842f2a960a709db557237
KENNICHE ABDERRAZAK <hash@KENNICHEs-MacBook-Air.local>
2023-09-29 20:19:35 +0100

    Init

---
+++ iMemory/iMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMemoryApp.swift
+//  iMemory
+//
+//  Created by KENNICHE ABDERRAZAK on 29/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct iMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
